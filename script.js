e808e1e0c6d3a210142be32eb5b36d6334b09e4e
dhruba619 <dhruba619@gmail.com>
2014-08-14 17:02:17 +0530

    Simple Calculator

---
+++ script.js
@@ -0,0 +1,43 @@
+function multiply(){
+				var firstValue = 0;
+				var secondValue = 0;
+				firstValue = document.getElementById("input1").value;
+				secondValue = document.getElementById("input2").value;
+				var result = firstValue * secondValue;
+				document.getElementById("input4").value = result;
+				}
+				
+function divide(){
+				var firstValue = 0;
+				var secondValue = 0;
+				firstValue = document.getElementById("input1").value;
+				secondValue = document.getElementById("input2").value;
+				var result = firstValue / secondValue;
+				document.getElementById("input4").value = result;
+				}
+
+				
+function add(){
+				var firstValue = 0;
+				var secondValue = 0;
+				firstValue = document.getElementById("input1").value;
+				secondValue = document.getElementById("input2").value;
+				var result = (firstValue+secondValue);
+				document.getElementById("input4").value = result;
+				}
+
+				
+function subtract(){
+				var firstValue = 0;
+				var secondValue = 0;
+				firstValue = document.getElementById("input1").value;
+				secondValue = document.getElementById("input2").value;
+				var result = firstValue - secondValue;
+				document.getElementById("input4").value = result;
+				}
+
+
+
+
+
+
